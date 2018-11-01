2b6f2e4534a6b16c33e8237d08319c74b5d776bb
Jackson Ho <mintri1199@gmail.com>
2018-10-31 19:07:25 -0700

    Users can delete comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -10,4 +10,16 @@ app.post('/reviews/comments', (req, res) => {
     })
 })
 
+// Delete
+app.delete("/reviews/comments/:id", (req, res) => {
+    console.log("Delete comment");
+    Comment.findByIdAndRemove(req.params.id)
+    .then((comment) => {
+        res.redirect(`/reviews/${comment.reviewId}`)
+    }).catch((err) => {
+        console.log(err.message);
+    })
+})
+
+
 module.exports = app
