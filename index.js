e0d34317bfb134492a349dd5265c7203a3ed0580
Jackson Ho <mintri1199@gmail.com>
2018-10-31 17:15:17 -0700

    User can see comments on a review

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const reviews = require('./controllers/reviews');
 const port =  process.env.PORT || 3000;
 
 
-// Middleware               // Use the github code as reference // must match the origin master
-                            // Error: local db is rotten-potatoes where git url is Rotten-Potatoes
-                            // Thus it tries to create but stop since both has simliar cases 
+// Middleware
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/rotten-potatoes", { useNewUrlParser: true})
 app.engine("handlebars", exphbs({defaultLayout: "main"}))
 app.set('view engine', 'handlebars');
