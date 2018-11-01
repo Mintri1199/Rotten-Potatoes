ccd4a521731f2eedd1bfca204230894e2e13adfc
Jackson Ho <mintri1199@gmail.com>
2018-10-31 17:12:49 -0700

    Revised

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ const reviews = require('./controllers/reviews');
 const port =  process.env.PORT || 3000;
 
 
-// Middleware
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Rotten-Potatoes", { useNewUrlParser: true})
+// Middleware               // Use the github code as reference // must match the origin master
+                            // Error: local db is rotten-potatoes where git url is Rotten-Potatoes
+                            // Thus it tries to create but stop since both has simliar cases 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/rotten-potatoes", { useNewUrlParser: true})
 app.engine("handlebars", exphbs({defaultLayout: "main"}))
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true}));
